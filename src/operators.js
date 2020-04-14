b99c48373b8060cf1e08a7afa9aed4d4eafce8c6
riajuneja256 <36851802+riajuneja256@users.noreply.github.com>
2020-04-14 20:08:40 +0530

    Update operators.js

--- src/operators.js
+++ src/operators.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { onClickOperator } from './actions';
 
-class Operators extends React.Component
-{
-    render(){
-        return(
+class Operators extends React.Component {
+    render() {
+        return (
             <div onClick={() => this.props.onClickOperator(this.props.data)} className="col s1 btn push-s3 button_style">{this.props.data}</div>
         );
     }
 }
-export default Operators;
+export default connect(
+    state => ({}),
+    dispatch => ({
+        onClickOperator: bindActionCreators(onClickOperator, dispatch)
+    })
+)(Operators);

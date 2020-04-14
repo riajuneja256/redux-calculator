a657dc214a418f7e9891b0278986b1231ec88216
riajuneja256 <36851802+riajuneja256@users.noreply.github.com>
2020-04-14 20:07:12 +0530

    Update display.jsx

--- src/display.jsx
+++ src/display.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import './calc.css';
+import { connect } from 'react-redux';
 
-class Display extends Component{
-   
-    render(){
-        return(
+class Display extends Component {
+
+    render() {
+        return (
             <React.Fragment>
-                <div  id="display" className="common_style right-align padding_0 display col s5 push-s3">
+                <div id="display" className="common_style right-align padding_0 display col s5 push-s3">
                     <input
                         className="right-align display"
                         type="text"
-                        placeholder="0"
-                        />
+                        value={this.props.expression.display}
+                        readOnly
+                    />
                 </div>
             </React.Fragment>
         );
     }
 
 }
-export default Display;
- 
+export default connect(
+    state => ({ expression: state.Modal }),
+    dispatch => ({})
+)(Display);

29c813cfd42ac90e43ac82611b82a25ecb3d6d83
riajuneja256 <36851802+riajuneja256@users.noreply.github.com>
2020-04-14 20:09:16 +0530

    Update redux.js

--- src/redux.js
+++ src/redux.js
@@ -1,12 +1,12 @@
 import { combineReducers, createStore } from 'redux';
-import {Modal} from './reducers';
+import { Modal } from './reducers';
 export const reducers = combineReducers({
   Modal,
 });
 
 // store.js
 export function configureStore(initialState = {}) {
-  const store = createStore(reducers, initialState);
+  const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   return store;
 };
 

41eaabfffe8e0112f44c7ed7d68f27cb58e17cf5
riajuneja256 <36851802+riajuneja256@users.noreply.github.com>
2020-04-14 20:07:36 +0530

    Update equals.js

--- src/equals.js
+++ src/equals.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
-class Equals extends React.Component
-{
-    render(){
-        return(
-            <div onClick={() => this.props.onClickEqual(this.props.data)} className="col s1 btn push-s3 button_style">{this.props.data}</div>
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { onClickEqual } from './actions';
+class Equals extends React.Component {
+    handleKey = () => {
+            
+            let sum = eval(this.props.state).toString();
+            console.log(sum);
+            this.props.onClickEqual(sum);
+    }
+    render() {
+        return (
+            <div onClick={() => this.handleKey()} className="col s1 btn push-s3 button_style">{this.props.data}</div>
         );
     }
 }
-export default Equals;
+
+const mapStateToProps = (state) => {    
+    console.log(state)
+    return {
+      state: state.Modal.display
+    };
+  }
+
+export default connect(
+    mapStateToProps,
+    dispatch => ({
+        onClickEqual: bindActionCreators(onClickEqual, dispatch)
+    })
+)(Equals);

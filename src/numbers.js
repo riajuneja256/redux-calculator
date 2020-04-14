2898184035b15d95250ba8d87bfa4f78cc47e97f
riajuneja256 <36851802+riajuneja256@users.noreply.github.com>
2020-04-14 20:08:19 +0530

    Update numbers.js

--- src/numbers.js
+++ src/numbers.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { onClickNumber } from './actions';
 
-class Button extends React.Component
-{
-    render(){
-        return(
-            <div onClick={() =>this.props.handlekey(this.props.data)} className="col s1 btn push-s3 button_style">{this.props.data}</div>
+class Button extends React.Component {
+    
+    render() {
+        return (
+            <div onClick={() => this.props.onClickNumber(this.props.data)} className="col s1 btn push-s3 button_style" > {this.props.data}</div>
         );
     }
 }
-export default Button;
+
+
+
+export default connect(
+    state => ({}),
+    dispatch => ({
+        onClickNumber: bindActionCreators(onClickNumber, dispatch)
+    })
+)(Button);
